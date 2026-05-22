a8cb7205e9220ab2955324c8ba09fabfbcbf3532
Adriano <adrianosenai604@gmail.com>
2026-05-22 17:49:42 -0300

    fix: alteração da rota base da api

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 async function chamarAPI() {
-  const resposta = await fetch("https://projeto-simples-back-teste.onrender.com")
+  const resposta = await fetch("https://projeto-simples-back-teste-2.onrender.com")
   const dados = await resposta.json()
   document.getElementById("resposta").textContent = dados.message
 }
